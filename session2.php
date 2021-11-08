c6772602f18ddc9fa59a069b43606bd7cb94b6cc
Ebrahim-Elsayed <cabtainhema30gmail.com>
2021-11-09 00:16:47 +0200

    task 2

---
+++ session2.php
@@ -0,0 +1,24 @@
+<?php
+
+/* * TASK
+Write a PHP function to print the next character of a specific character.
+input : 'a'
+Output : 'b'
+input : 'z'
+Output : 'a'
+*/
+
+// function next_char($char){
+//     return substr($char , 3 );
+// }
+// echo next_char("hello");
+
+$char = 'z';
+$next_char = ++$char; 
+if (strlen($next_char) > 1) {
+ $next_char = $next_char[0];
+}
+echo $next_char;
+
+
+?>
